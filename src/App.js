b2bced52fa4cba6d33d3ad85fd0a86bc247f76e1
Students-g16 <gruppag783@gmail.com>
2025-02-05 17:24:45 +0500

    done for using

--- src/App.js
+++ src/App.js
@@ -3,3 +3,5 @@ import React from "react";
 export default function App() {
   return <div>App</div>;
 }
+// https://www.youtube.com/watch?v=wYpCWwD1oz0&list=PLEYW3pZS6IQ_a-iYAno4VsZonrikphq8L
+// 7:12
